8faae74fc0a075b0d035d1498eb0365d9b5bc8ea
abdulaziz <almahdi@gmail.com>
2022-06-18 21:11:16 +0300

    init

---
+++ WebApi/Program.cs
@@ -0,0 +1,31 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using WebApi.Models;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<masterContext>(op => op.UseSqlServer("Data Source = THARWATEXAM\\SQL2019STD; Database = Cloth_Factory; User ID = sa; Password = Yemen@134;"));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
